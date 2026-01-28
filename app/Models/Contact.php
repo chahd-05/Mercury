1781df13aae741f2a00db90a7bac3f1aada76fab
shahd <mazouzchahd12@gmail.com>
2026-01-28 14:58:18 +0100

    create contact class

---
+++ app/Models/Contact.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Contact extends Model
+{
+   protected $fillable = [
+        "name",
+        "email",
+        "phone",
+        "group_id"
+   ];
+
+   public function group()
+   {
+       return $this->belongsTo(Group::class);
+   }
+}
+
+
